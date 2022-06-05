68c1e0cfe79b77932e3afdcf281c238724d8be49
aaronlrv <aaronvitharanage@gmail.com>
2022-06-05 12:35:23 +0400

    Add initial table grid function

---
+++ script.js
@@ -0,0 +1,20 @@
+function table(){
+    let table = document.querySelector(".container")
+
+    for (i = 0; i < 16; i++){
+    let row = document.createElement(tr)
+    row.id = "row" + i
+    let myRow = document.getElementById("row" + i)
+    
+
+    table.appendChild(row)
+
+    for (j = 0; j < 16; i++){
+        let myCell = document.createElement("td")
+
+        myRow.appendChild(myCell)
+    }
+}
+}
+
+table()

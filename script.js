114d8972a22c939b565b069d6b8ac6b86afcd64f
aaronlrv <aaronvitharanage@gmail.com>
2022-06-05 12:49:08 +0400

    Add header and description

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function table(){
     let table = document.querySelector(".container")
 
     for (i = 0; i < 16; i++){
-    let row = document.createElement(tr)
+    let row = document.createElement("tr")
     row.id = "row" + i
     let myRow = document.getElementById("row" + i)
     

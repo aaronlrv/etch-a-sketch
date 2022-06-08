8ad75fcf14433c27e8a975929395ee1bc71c5ce2
aaronlrv <aaronvitharanage@gmail.com>
2022-06-08 22:49:04 +0400

    Add a submit feature and a way for input to be captured from the user

--- script.js
+++ script.js
@@ -1,7 +1,21 @@
-function createTable (){
+function userInput (){
+    let rows = document.querySelector("#row")
+    let columns = document.querySelector("#column")
+
+    if (rows < 101 || columns < 101){
+        let rows = i
+        let columns = j
+    } else {
+        alert("Selection for row and columns were too high!")
+    }
+}
+
+
+
+function createTable (userInput){
 let table = document.querySelector(".grid-container")
 
-for (i = 0; i < 256; i++){
+for (i = 0; i * j < 10000; i++){
     let myRow = document.createElement("div")
     myRow.className = "gridBoxes"
     table.appendChild(myRow)

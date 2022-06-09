7a5697366f2c64e99a8dd1a9c16dd7745c240277
aaronlrv <aaronvitharanage@gmail.com>
2022-06-09 11:01:06 +0400

    Add if function to determine if input is too big

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
 function table () {
 
+    /* Takes user input converts it to number and gets a total*/
     let rows = document.querySelector("#row").value
     let columns = document.querySelector("#column").value
 
     let i = parseInt(rows)
     let j = parseInt(columns)
+
+    if (i > 100 || j > 100){
+        alert("Your selection is too big!")
+        return
+    }
 
     console.log(i)
     console.log(j)
@@ -14,17 +20,16 @@ function table () {
     let total = i* j
     console.log(total)
 
+    /* Creates x amount of divs based on total */
 let table = document.querySelector(".grid-container")
 
 for (k = 0; k < total; k++){
     let myRow = document.createElement("div")
     myRow.className = "gridBoxes"
     table.appendChild(myRow)
-
-
-
 }
 
+/*Listens for when user hovers over div and colors them by applying classes*/
 let divs = document.querySelectorAll(".gridBoxes")
 
 divs.forEach(div => 
@@ -32,6 +37,7 @@ div.addEventListener('mouseenter', function (){
 div.classList.add('black')
 
 }))
+
 }
 
 
